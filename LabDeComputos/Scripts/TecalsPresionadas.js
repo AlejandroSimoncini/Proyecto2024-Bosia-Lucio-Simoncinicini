d7594ebce4b5314e959faec863c25aefb42993c7
AlejandroSimoncini <alesimoncini05@gmail.com>
2024-04-13 01:34:29 -0300

    Upload
    Se pusieron funciones de aparicion de botones

--- LabDeComputos/Scripts/TecalsPresionadas.js
+++ LabDeComputos/Scripts/TecalsPresionadas.js
@@ -1,10 +1,20 @@
-document.addEventListener('keydown', function(event)
+function TeclaPresionada(evento){
+    if(evento.keyCode === 32)
     {
-     if(event.code === 'Space')
-    {
-        const Facil = document.querySelector('')
-        
-    }   
-    }
+        var elementosMenu = document.querySelectorAll('.menu');
 
-)
+        elementosMenu.forEach(function(elemento) {
+        elemento.style.opacity = '0.5';
+        elemento.style.filter = 'blur(10px)';
+        }
+    );
+        var elementosContainer = document.getElementById('BotonContainer')          /*Script de modificacion de la pagina al apretar la tecla espacio*/ 
+        elementosContainer.style.display = 'flex';
+        elementosContainer.style.visibility = 'visible';
+
+
+        
+    }
+}
+
+document.addEventListener('keydown', TeclaPresionada);
