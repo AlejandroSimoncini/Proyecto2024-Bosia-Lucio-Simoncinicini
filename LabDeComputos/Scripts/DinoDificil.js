5e056159b20e2184f96c8cd024d48645ef7319f4
TomasELucio <115887577+TomasLucio@users.noreply.github.com>
2024-05-08 09:32:10 -0300

    dificultades agregadas

---
+++ LabDeComputos/Scripts/DinoDificil.js
@@ -0,0 +1,141 @@
+//board
+let board;
+let boardHeight = 250;
+let boardWidth = 700;
+
+
+let context; //variable usada para dibujar sobre el canvas
+
+//dino
+let dinoWidth=70;
+let dinoHeight=80;
+let dinoX=50;
+let dinoY=boardHeight-dinoHeight; //altura del tablero - altura del dinosaurio
+let dinoImg;
+
+let dino={
+    x : dinoX,
+    y : dinoY,
+    width : dinoWidth,
+    height : dinoHeight,
+}
+
+//cactus
+let cactusArray=[];
+let cactus1Width=34;
+let cactus2Width=60;
+let catusHeight=70;
+let cactusX=700;
+let cactusY=boardHeight-catusHeight;
+let cactus1Img;
+let cactus2Img;
+
+//fisicas
+let velocidadX= -8;
+let velocidadSalto = 0;
+let gravedad = 1.5;
+
+let gameOver;
+
+window.onload = function() //
+{
+    board = document.getElementById("board");
+    board.height=boardHeight;
+    board.width=boardWidth;
+
+    context = board.getContext("2d"); //se usa para dibujar en el board
+
+    dinoImg= new Image();
+    dinoImg.src = "../Imagenes/Dino.gif";
+    dinoImg.onload = function()
+    {
+        context.drawImage(dinoImg, dino.x, dino.y, dino.width , dino.height);
+    }
+
+    cactus1Img = new Image();
+    cactus1Img.src = "../Imagenes/Cactus.gif";
+
+    cactus2Img = new Image();
+    cactus2Img.src = "../Imagenes/TresCactus.gif";
+    
+    requestAnimationFrame(actualizar);
+    setInterval(crearCactus, 1000); //llama a la funcion crear cactus cada 1000 milisegundos = 1 segundo
+    document.addEventListener("keydown", saltar);
+}
+
+function actualizar()  //funcion que dibuja cada frame
+{
+    requestAnimationFrame(actualizar);
+    
+    if (gameOver) {
+        return;
+    }
+
+    context.clearRect(0,0,boardWidth,boardHeight); //elimina los movimientos anteriores de cada elemento
+    
+    velocidadSalto += gravedad;
+    dino.y = Math.min(dino.y + velocidadSalto, dinoY); //agrega la velocidad y asegura que el dinosaurio no salga del board
+    context.drawImage(dinoImg, dino.x, dino.y, dino.width , dino.height);
+
+    for (let i=0 ;i <cactusArray.length; i++)
+    {
+        let cactus=cactusArray[i];
+        cactus.x+=velocidadX;
+        context.drawImage(cactus.img , cactus.x , cactus.y , cactus.width , cactus.height);
+        
+        if (colision(dino,cactus)) 
+        {
+            gameOver = true;
+            dinoImg.onload = function(){
+                context.drawImage(dinoImg, dino.x, dino.y, dino.width , dino.height);
+            }
+        }
+    }
+
+}
+
+function crearCactus()
+{
+    let cactus={
+        img : null,
+        x : cactusX,
+        y : cactusY,
+        width : null,
+        height : catusHeight
+    }
+    //asignamos el valor null a img y width ya que dependen de cada tipo de cactus
+
+    let crearCactusRand = Math.random(); //devuelve funciones entre 0 y 1
+    
+    if (crearCactusRand>0.80) 
+    {
+        cactus.img=cactus2Img;
+        cactus.width=cactus2Width;
+        cactusArray.push(cactus);
+    }
+    else if (crearCactusRand>0.50) 
+    {
+        cactus.img=cactus1Img;
+        cactus.width=cactus1Width;
+        cactusArray.push(cactus);
+    }
+
+    if (cactusArray.length > 5) {
+        cactus.shift(); //elimina el primer elemento del arreglo para que no se acumule en la memoria
+    }
+}
+
+function saltar(event)
+{
+    if ((event.code == "Space" || event.code == "ArrowUp") && dino.y===dinoY) {
+        velocidadSalto = -20;
+    }
+}
+
+function colision(din,cac)
+{
+    return din.x < cac.x + cac.width && //esquina superior izquierda del dino / esquina superior derecha catus
+        din.x + dino.width > cac.x &&  //esquina superior derecha dino / esquina superior izquirda cactus
+        din.y < cac.y + cac.width &&  // esquina superior izquierda dino / esquina inferior izquierda cactus
+        din.y + din.height > cac.y;  // esquina inferior izquierda dino / esquina superior izquierda cactus
+}
